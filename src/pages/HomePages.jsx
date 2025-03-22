6a7937771fc7b50098c4abc2eff70c8c7748c7e2
marrasluca <lmarras10@gmail.com>
2025-03-22 16:43:54 +0100

    add: function fetch

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -1,13 +1,42 @@
 import MovieCard from "../components/MovieCard"
 import ReviewCard from "../components/ReviewCard"
+import axios from "axios"
+import {useState, useEffect} from 'react'
 
 export default function HomePage(){
+
+    const [ movies, setMovies ] = useState([])
+
+    //funzione fetch
+    const fetchMovies = () => {
+        console.log ( 'Fetching movies...' )
+
+        axios.get('http://localhost:3001/movies')
+        .then( res => { setMovies ( res.data ) } )
+        .catch ( error => { console.log(error) } )
+    }
+
+    const renderMovies = () => {
+        return(
+            movies.map( (movie) => {
+                return(
+                    <div className="col" key={movie.id}>
+                        <MovieCard movie={movie}/>
+                    </div>
+                )
+            })
+        )
+    }
+
+    useEffect( fetchMovies, []) 
+
     return(
        <>
         <h1 className="text-primary">Movies</h1>
 
         <div className="row row-cols-3">
-           <MovieCard/>
+           {/* <MovieCard/> */}
+           { renderMovies() }
         </div>
         </> 
     )
